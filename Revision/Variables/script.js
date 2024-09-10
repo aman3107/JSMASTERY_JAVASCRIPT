107c7f629465b2c8b8774a49e7132548479bd31c
Aman Mittal <amanmittalma@gmail.com>
2024-09-11 00:02:01 +0530

    first commit

---
+++ Revision/Variables/script.js
@@ -0,0 +1,17 @@
+// console.log("Hello World");
+// console.log("Welcome to Variables");
+
+// var variableName = "Welcome to Variables";
+// console.log(variableName);
+
+// let variableName1 = "Welcome to Variables";
+// console.log(variableName1);
+// variableName1 = "Hello";
+// console.log(variableName1);
+
+// const variableName2 = "Welcome to Variables";
+// console.log(variableName2);
+// // variableName2 = "Hello";
+// console.log(variableName2);
+
+/* Strings */
