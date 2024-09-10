119d1a817a70e25e9937a3e9155a3753e2264c27
Aman Mittal <amanmittalma@gmail.com>
2024-09-11 01:42:14 +0530

    Operators

---
+++ Revision/Operators/script.js
@@ -0,0 +1,102 @@
+/* Arithmetic Operators */
+
+// const a = 5;
+// const b = 10;
+
+// let result = 0;
+
+// addition
+// result = a + b;
+// console.log(result);
+
+// subtraction
+// result = a - b;
+// console.log(result);
+
+// multiplication
+// result = a * b;
+// console.log(result);
+
+//division
+// result = a / b;
+// console.log(result);
+
+//exponent
+// result = a ** b;
+// console.log(result);
+
+// module
+// result = 13 % 12;
+// console.log(result);
+
+// increment
+// result++;
+// console.log(result);
+
+// decrement
+// result--;
+// console.log(result);
+
+/* Comparison Operators and Equality */
+
+// const a = 10;
+// const b = 10;
+
+// greater than
+// console.log(a > b);
+
+// greater than equals to
+// console.log(a >= b);
+
+// less than
+// console.log(a < b);
+
+// lest than equals to
+// console.log(a <= b);
+
+// Is Equal
+// console.log(a === b);
+
+// not equal
+// console.log(a !== b);
+
+/* Strict vs Loose Equality */
+
+// Loose Equality
+// console.log(0 == "");
+// console.log("" == "0");
+// console.log(0 == "0");
+// console.log(false == "0");
+
+// console.log("---------------------");
+
+// Strict Equaltiy
+// console.log(0 === "");
+// console.log("" === "0");
+// console.log(0 === "0");
+// console.log(false === "0");
+
+/* Logical Operators */
+
+// AND
+// console.log(true && true);
+// console.log(true && true && false);
+
+// OR
+// console.log(true || false);
+// console.log(true || false || false);
+
+// NOT
+// console.log(!true);
+
+/* Assignment Operator */
+
+let number = 5;
+number += 5;
+console.log(number);
+number -= 5;
+console.log(number);
+number *= 5;
+console.log(number);
+number /= 5;
+console.log(number);
--- Revision/Variables/script.js
+++ Revision/Variables/script.js
@@ -15,3 +15,55 @@
 // console.log(variableName2);
 
 /* Strings */
+
+// const doubleQuotes = "Hello";
+// const name = "Aman";
+// const backTicks = `Hello ${name}`;
+// console.log(backTicks);
+
+/* Numbers */
+
+// const wholeNumber = 10;
+// const decimalNumber = 0.5;
+
+// const firstNumber = 5;
+// const secondNumber = 10;
+
+// const addition = firstNumber + secondNumber;
+// console.log(addition);
+// const subtraction = firstNumber - secondNumber;
+// console.log(subtraction);
+// const divide = firstNumber / secondNumber;
+// console.log(divide);
+// const multiply = firstNumber * secondNumber;
+// console.log(multiply);
+
+/* Booleans */
+
+// const iscool = false;
+// console.log(iscool);
+// if (iscool) {
+//   console.log("Hi, you're cool");
+// } else {
+//   console.log("Ohh, hi!");
+// }
+
+/* Null and Undefined */
+
+// let age = null;
+// age = 20;
+// console.log(age);
+
+// let x;
+// console.log(x);
+
+/* Objects */
+
+const person = {
+  name: "Aman",
+  age: 20,
+};
+
+console.log(person);
+console.log(person["name"]);
+console.log(typeof person.name);
