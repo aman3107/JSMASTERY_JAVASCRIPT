e09be330094a67ea9d9e0e6e3ff3afef273db350
Aman Mittal <amanmittalma@gmail.com>
2024-10-01 00:03:01 +0530

    Tricky Parts

---
+++ Revision/Tricky Parts/script.js
@@ -0,0 +1,28 @@
+console.log("Hello World");
+
+// Global Scope
+
+// const name = "John";
+// const logName = () => {
+//   console.log(`Hi, ${name}`);
+// };
+
+// logName();
+
+// Local Scope or Function Scope
+
+// const someFunction = () => {
+//   const name = "Jenny";
+//   console.log(name);
+// };
+// console.log(name);
+// someFunction();
+
+// Hoisting in JavaScript
+console.log(age);
+var age = 20;
+
+hoist();
+function hoist() {
+  console.log("Hello World");
+}
