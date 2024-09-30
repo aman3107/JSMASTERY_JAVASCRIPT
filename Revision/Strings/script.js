1c495878212610388185624a8553b69138daa5b4
Aman Mittal <amanmittalma@gmail.com>
2024-10-01 00:40:15 +0530

    Strings

---
+++ Revision/Strings/script.js
@@ -0,0 +1,20 @@
+// console.log("Hello World");
+const double = "This is a double string";
+const backTick = `This is a backtick string`;
+console.log(double);
+console.log(backTick);
+
+const add = `${2 + 2}`;
+console.log(add);
+
+const sum = (a, b) => a + b;
+const total = `The total is ${sum(2, 2)}`;
+console.log(total);
+
+const rows = `
+1
+2
+3
+`;
+
+console.log(rows);
