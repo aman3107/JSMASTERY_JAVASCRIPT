2c9451cb92a82406254a190301f732273f41cb8c
Aman Mittal <amanmittalma@gmail.com>
2024-10-07 14:19:44 +0530

    Arrays Methods

--- Revision/Arrays/script.js
+++ Revision/Arrays/script.js
@@ -80,21 +80,79 @@
 // console.log(items);
 
 /* Array filter */
-const numbers = [-10, 0, -2, 15, -36, 25];
-const even = numbers.filter((number) => number >= 0);
-console.log(even);
+// const numbers = [-10, 0, -2, 15, -36, 25];
+// const even = numbers.filter((number) => number >= 0);
+// console.log(even);
 
-const employeesData = [
-  { name: "Sebastian", overTime: 5 },
-  { name: "Cardi Vee", overTime: 10 },
-  { name: "George Lopez", overTime: 12 },
-];
+// const employeesData = [
+//   { name: "Sebastian", overTime: 5 },
+//   { name: "Cardi Vee", overTime: 10 },
+//   { name: "George Lopez", overTime: 12 },
+// ];
 
-const employeesToReward = employeesData.filter(
-  (employee) => employee.overTime >= 7
-);
+// const employeesToReward = employeesData.filter(
+//   (employee) => employee.overTime >= 7
+// );
 
-const employeesName = employeesToReward.map((employee) => employee.name);
-console.log(employeesToReward, employeesName);
+// const employeesName = employeesToReward.map((employee) => employee.name);
+// console.log(employeesToReward, employeesName);
 
-employeesName.forEach((name) => console.log(`${name} received an reward`));
+// employeesName.forEach((name) => console.log(`${name} received an reward`));
+
+/* Array Find */
+
+// const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const values = numbers.find((number) => number > 5);
+// console.log(values);
+
+// const states = ["Alaska", "California", "Colorado", "Hawaii"];
+// const startsWithC = states.find((state) => state.startsWith("C"));
+// console.log(startsWithC);
+
+/* Array Includes */
+
+// const array1 = [1, 2, 3];
+// console.log(array1.includes(1));
+
+// const pets = ["cat", "dog", "llama"];
+// console.log(pets.includes("cat"));
+
+/* Array Sort */
+
+// const names = ["Anne", "Carl", "Bob", "Dean"];
+// names.sort();
+// console.log(names);
+
+// const numbers = [6, 3, 1, 7, 9, 2, 15, 22, 77, 99];
+
+// // Ascending Order
+// numbers.sort((a, b) => a - b);
+// console.log(numbers);
+
+// // Descending Order
+// numbers.sort((a, b) => b - a);
+// console.log(numbers);
+
+/* Array some and every */
+
+// const array = [1, 2, 3, 4, 5];
+
+// console.log(array.some((number) => number > 3));
+
+// console.log(array.every((number) => number > 0));
+
+/* Array Reduce */
+
+const groceryList = [29, 12, 45, 35, 87, 110];
+// let sum = 0;
+// groceryList.forEach((price) => {
+//   sum += price;
+// });
+// console.log(sum);
+
+const price = groceryList.reduce((acc, curr) => {
+  return acc + curr;
+});
+
+console.log(price);
