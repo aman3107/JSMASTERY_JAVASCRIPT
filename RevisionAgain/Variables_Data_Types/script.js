ebe7b473b7d11f654d41600eaf72369b55c35550
Aman Mittal <amanmittalma@gmail.com>
2025-04-15 16:21:43 +0530

    Data Types In JavaScript

---
+++ RevisionAgain/Variables_Data_Types/script.js
@@ -0,0 +1,61 @@
+// console.log("Hello World");
+
+// var name1 = "Hello";
+// console.log(name1);
+
+// let name2 = "World";
+// console.log(name2);
+
+// const name3 = "Ji";
+// console.log(name3);
+
+/* Strings */
+
+// const doubleQuote = "Hello World";
+// const name = "Aman";
+// const backTicks = `Hello, ${name} welcome `;
+// console.log(backTicks);
+
+/* Numbers */
+
+// const wholeNumber = 5;
+// const decimalNumber = 5.5;
+// console.log(wholeNumber);
+// console.log(decimalNumber);
+
+/* Booleans */
+
+// const isCool = true;
+// console.log(isCool);
+// const age = 18;
+// if (age >= 18) {
+//   console.log(`You may enter`);
+// }
+
+/* NULL */
+
+// let a = null;
+// console.log(a);
+// a = 20;
+// console.log(a);
+
+/* Undefined */
+
+// let x;
+// console.log(x);
+
+/* Objects */
+
+const person = {
+  name: "john",
+  age: 25,
+};
+console.log(person.name);
+
+/* Arrays */
+
+const arr = [1, 2, 3];
+console.log(arr);
+
+const date = new Date();
+console.log(date);
